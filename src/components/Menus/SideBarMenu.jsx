0c7b8c586e515caa05e6c7e094260ce6744d07ba
divine <divineosuuweb@gmail.com>
2024-04-09 15:42:49 +0200

    Adding sideBar After user login

--- src/components/Menus/SideBarMenu.jsx
+++ src/components/Menus/SideBarMenu.jsx
@@ -2,16 +2,20 @@ import '../../style/SideBarMenu.css';
 import notifications from '../../assets/svg/notification.svg';
 import messages from '../../assets/svg/comment-solid.svg';
 import settings from '../../assets/svg/settings-solid.svg';
+import aliasIcon from '../../assets/svg/earth-africa-solid.svg';
 
 
 const SideBar = () => {
-    return <div>
-        <div>
+    return <div className='side-bar-container'>
+        <div className='upper-side-bar-icon-container-flex'>
             <div className='upper-side-bar-icon-container'><img src={notifications} alt="" /></div>
             <div className='upper-side-bar-icon-container'><img src={messages} alt="" /></div>
             <div className='upper-side-bar-icon-container'><img src={settings} alt="" /></div>
         </div>
-        <div></div>
+        <div className='alias-icon'>
+            <img src={aliasIcon} alt="Picture of the globe oriented towards Africa" />
+
+        </div>
     </div>
 }
 
