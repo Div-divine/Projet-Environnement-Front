5f33e90a2921aca9a10e5489039830ee0ddaf53a
divine <divineosuuweb@gmail.com>
2024-06-27 19:19:06 +0200

    Modify connected user homepage

--- src/components/home-page/HomePage.jsx
+++ src/components/home-page/HomePage.jsx
@@ -26,14 +26,14 @@ const RenderHome = () => {
 
     useEffect(() => {
         if (location.pathname === 'accueil') {
-            setCounter(counter+1)
+            setCounter(counter + 1)
         }
     }, [location])
 
-    useEffect(()=>{
-       if(counter === 1){
-        window.location.reload()
-       }
+    useEffect(() => {
+        if (counter === 1) {
+            window.location.reload()
+        }
     }, [counter])
 
     useEffect(() => {
@@ -83,62 +83,64 @@ const RenderHome = () => {
         <main className="main-elements">
             <DisplayConnectedSmallMenu />
             <div className="main-body-container">
-                <div className="text-center welcom-msg-container">
-                    <p className="welcom-msg">Merci de nous avoir rejoint dans la mission de sauver la planète 🌍! </p>
-                    <p className="welcom-msg"><span className="welcom-msg-green">Vos actions font toute la différence</span></p>
-                </div>
-                <div className="group-upper-title-container">
-                    <p className="group-upper-title">Groups d'actions</p>
-                </div>
-                <div className="groups-container">
-                    {groupsDatas && groupsDatas.map((data) => {
-                        return (
-                            <div className="water-container mb-3" key={data.group_id} >
-                                <div className="icon-and-btns-container">
-                                    <div className="water-icon-and-text-conatiner">
-                                        <div className="water-icon-conatiner">
-                                            <img src={`../../src/${data.group_icon}`} alt="water icon" />
+                <div>
+                    <div className="text-center welcom-msg-container">
+                        <p className="welcom-msg">Merci de nous avoir rejoint dans la mission de sauver la planète 🌍! </p>
+                        <p className="welcom-msg"><span className="welcom-msg-green">Vos actions font toute la différence</span></p>
+                    </div>
+                    <div className="group-upper-title-container">
+                        <p className="group-upper-title">Groups d'actions</p>
+                    </div>
+                    <div className="groups-container">
+                        {groupsDatas && groupsDatas.map((data) => {
+                            return (
+                                <div className="water-container mb-3" key={data.group_id} >
+                                    <div className="icon-and-btns-container">
+                                        <div className="water-icon-and-text-conatiner">
+                                            <div className="water-icon-conatiner">
+                                                <img src={`../../src/${data.group_icon}`} alt="water icon" />
+                                            </div>
+                                            <div className="water-text-container">
+                                                <p className="water-resources-text">{data.group_name}</p>
+                                            </div>
                                         </div>
-                                        <div className="water-text-container">
-                                            <p className="water-resources-text">{data.group_name}</p>
+                                        <div className="add-and-viste-icon-and-text-container">
+                                            <motion.div whileTap={{ scale: 1.2 }}
+                                                className="add-and-text-conatiner"
+                                                onClick={(e) => {
+                                                    setPostId(data.group_id)
+                                                }}>
+                                                <Link className="add_icon_container">
+                                                    <img src={addIcon} alt="" />
+                                                </Link>
+                                                <Link className="add-text-container">
+                                                    <p className="texts">Ajouter</p>
+                                                </Link>
+                                            </motion.div>
+
+                                            <Link to={`/${data.group_name.toLowerCase().replace(/ /g, '-')}/${data.group_id}${data.group_uuid}`} className="visit-container">
+                                                <div className="visit-icon-container">
+                                                    <img src={eyeIcon} alt="" />
+                                                </div>
+                                                <div className="visit-text-container">
+                                                    <p className="texts">Visiter</p>
+                                                </div>
+                                            </Link>
+
                                         </div>
                                     </div>
-                                    <div className="add-and-viste-icon-and-text-container">
-                                        <motion.div whileTap={{ scale: 1.2 }}
-                                            className="add-and-text-conatiner"
-                                            onClick={(e) => {
-                                                setPostId(data.group_id)
-                                            }}>
-                                            <Link className="add_icon_container">
-                                                <img src={addIcon} alt="" />
-                                            </Link>
-                                            <Link className="add-text-container">
-                                                <p className="texts">Ajouter</p>
-                                            </Link>
-                                        </motion.div>
-
-                                        <Link to={`/${data.group_name.toLowerCase().replace(/ /g, '-')}/${data.group_id}${data.group_uuid}`} className="visit-container">
-                                            <div className="visit-icon-container">
-                                                <img src={eyeIcon} alt="" />
-                                            </div>
-                                            <div className="visit-text-container">
-                                                <p className="texts">Visiter</p>
-                                            </div>
-                                        </Link>
-
+                                    <div className="action-text-container">
+                                        <div className="text-center action-text"><p>Actions</p></div>
+                                        <div className="action-description-container">
+                                            <p className="text-center">-{data.group_action.split(',')[0]}</p>
+                                            {data.group_action.split(',')[1] && <p className="text-center mt-2">-{data.group_action.split(',')[1]}</p>}
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="action-text-container">
-                                    <div className="text-center action-text"><p>Actions</p></div>
-                                    <div className="action-description-container">
-                                        <p className="text-center">-{data.group_action.split(',')[0]}</p>
-                                        {data.group_action.split(',')[1] && <p className="text-center mt-2">-{data.group_action.split(',')[1]}</p>}
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    }
-                    )}
+                            )
+                        }
+                        )}
+                    </div>
                 </div>
             </div>
         </main>
